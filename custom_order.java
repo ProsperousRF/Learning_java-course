b2fc93bccb0a25008a605e0611864252697d825e
Stanislav Rakitov <prosperousrf@gmail.com>
2021-08-01 20:30:59 +0300

    Step 9

--- custom_order.java
+++ custom_order.java
@@ -113,7 +113,15 @@ System.out.print("\n"); // skips a line
       
     // STEP 9 DISPLAY ORDER CONFIRMATION
 
-
+    System.out.println();
+    System.out.println(firstName + " , your order is as follows: ");
+    System.out.println("_________________________________________");
+    System.out.println("Item Ordered: " + itemOrder);
+    System.out.println("Frosting: " + frostingType);
+    System.out.println("Filling: " + fillingType);
+    System.out.println("Toppings: " + toppings);
+    System.out.println("_________________________________________");
+    
       
 	  
 	  
